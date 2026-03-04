d5377d12df762bd4b574e616251d8f1b5c7a4ffa
Vyas <nirav.hiral@gmail.com>
2026-03-04 17:27:32 +0530

    Initialize partnership_form.js with basic logging

---
+++ partnership_form.js
@@ -0,0 +1,4 @@
+console.log("Loaded: partnership_form.js");
+
+// Put your renderSHForm / setTA / setTP / setTT / etc functions here.
+// IMPORTANT: this file must not contain </script> or </body> or </html>
